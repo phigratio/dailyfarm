5f09a733835b50e7f0347e45accf9c0f1227b4a4
phigratio <muqtadir.mujahid02@gmail.com>
2025-06-28 20:10:18 +0600

    Implemented Feign Client within microservices

---
+++ backend/FarmService/src/main/java/com/dailyfarm/Farm/FarmService/dto/UserDto.java
@@ -0,0 +1,2 @@
+package com.dailyfarm.Farm.FarmService.dto;public class UserDto {
+}
---
+++ backend/UserService/src/main/java/com/dailyfarm/user/service/UserService/external/services/FarmServiceClient.java
@@ -0,0 +1,19 @@
+package com.dailyfarm.user.service.UserService.external.services;
+
+import com.dailyfarm.user.service.UserService.dto.FarmDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "FARM-SERVICE", url = "${farm.service.url:http://localhost:8081}")
+public interface FarmServiceClient {
+
+    @GetMapping("/farms/owner/{ownerId}")
+    ResponseEntity<List<FarmDto>> getFarmsByOwnerId(@PathVariable("ownerId") String ownerId);
+
+    @GetMapping("/farms/{farmId}")
+    ResponseEntity<FarmDto> getFarmById(@PathVariable("farmId") String farmId);
+}
